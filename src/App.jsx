91d24911457364599e42033d6600db385281b978
Rezwanul Haque <74784389+REZWANUL1@users.noreply.github.com>
2024-01-24 18:07:43 +0600

    adding experience section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./components/Navigation/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 				<Navbar></Navbar>
 				<Hero></Hero>
 				<About></About>
+				<Experience />
 			</div>
 		</>
 	);
