e939d8d29fdac5315ca6a461bde7a3411c5124bd
Rezwanul Haque <74784389+REZWANUL1@users.noreply.github.com>
2024-01-24 18:42:54 +0600

    adding projects section

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Navbar from "./components/Navigation/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
+import Projects from "./components/Projects/Projects";
 
 function App() {
 	return (
 		<>
 			<div className={styles.App}>
-				<Navbar></Navbar>
-				<Hero></Hero>
-				<About></About>
+				<Navbar />
+				<Hero />
+				<About />
 				<Experience />
+				<Projects />
 			</div>
 		</>
 	);
